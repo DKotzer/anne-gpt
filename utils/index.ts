d0488298ecd1134897edc9cbbc146cc67dcb4ec5
Dylan <34380246+DKotzer@users.noreply.github.com>
2024-10-19 03:00:14 -0400

    strings

--- utils/index.ts
+++ utils/index.ts
@@ -15,7 +15,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl as string, supabaseKey as string)
 
 export const OpenAIStream = async (prompt: string, apiKey: string) => {
   const encoder = new TextEncoder();
